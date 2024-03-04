ca543f663ebca9e6b3cf56a582ed5f1f3bc3e151
Aayush Bharti <aayushbh2002@gmail.com>
2024-03-04 18:21:30 +0530

    update

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -2,19 +2,25 @@ import Card from "./Card";
 
 function Tours({ tours, removeTour }) {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <div className="flex justify-center items-center">
-        <h2 className="border-purple-950 px-16 py-4 border-dashed border-8 
-        font-bold text-4xl conte text-center rounded-xl my-8">Plan With Aayush</h2>
+        <h2
+          className="border-purple-950 px-16 py-4 border-dashed border-8 
+        font-bold text-4xl conte text-center rounded-xl my-8"
+        >
+          Plan With Aayush
+        </h2>
       </div>
-      <div className="flex justify-center flex-wrap max-w-[1300px]
-      my-auto mx-0">
-        {
-            tours.map((tour) => {
-            return <Card {...tour} removeTour={removeTour}></Card>;
-            //here spread op used to pass a clone of tours
-          })
-        }
+      <div
+        className="flex justify-center flex-wrap max-w-[1300px]
+      my-auto mx-0"
+      >
+        {tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+          //key={tour.id} pass a key for good practice while using maps
+          //good rule of thumb is that elements inside the map( ) call need keys.
+          //here spread op used to pass a clone of tours
+        })}
       </div>
     </div>
   );
